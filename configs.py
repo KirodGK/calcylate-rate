9dd70ab089da5d53c4546d534dbf57fdf94b3d5a
Dmitriev Kirill 'KirodGK <KirillDm2001@yandex.ru>
2025-06-02 22:07:27 +0300

    first

---
+++ configs.py
@@ -0,0 +1,23 @@
+import argparse
+
+
+def configure_argument_parser(available_modes):
+    parser = argparse.ArgumentParser(description='Калькулятор зарплаты')
+    parser.add_argument(
+        'mode',
+        choices=available_modes,
+        help='Режимы работы'
+    )
+    # parser.add_argument(
+    #     '-c',
+    #     '--clear-cache',
+    #     action='store_true',
+    #     help='Очистка кеша'
+    # )
+    parser.add_argument(
+        '-r',
+        '--report',
+        choices=('pretty', 'file'),
+        help='Дополнительные способы вывода данных'
+    )
+    return parser
---
+++ main.py
@@ -0,0 +1,2 @@
+with open('./test/data1.csv', 'r') as file:
+    print(file.read().split('\n'))
